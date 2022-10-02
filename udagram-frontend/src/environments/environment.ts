f037f8b52908d09041f10c2f6f3c5e9053ec8b00
Ochiabuto Jideofor <chukajide@gmail.com>
2022-10-02 01:07:51 +0100

    building and deployment done

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://afd14f9781ae047aebf83afc5c1fc9df-969116142.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
